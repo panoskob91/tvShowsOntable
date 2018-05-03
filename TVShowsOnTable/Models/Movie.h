79f882a3c96989e9eb65ef430b7c053f61ac8b6b
panoskob91 <panoskob91@gmail.com>
2018-05-03 09:06:04 +0300

    feat: Sections based on genre completed, enums removed

--- TVShowsOnTable/Models/Movie.h
+++ TVShowsOnTable/Models/Movie.h
@@ -14,32 +14,6 @@
 @property (strong, nonatomic) NSString *movie;
 //@property (strong, nonatomic) NSString *summary;
 
-//#pragma mark -MovieGenre enum
-//enum MovieGenre
-//{
-//    
-//    MVAction = 28,
-//    MVAdventure = 12,
-//    MVAnimation = 16,
-//    MVComedy = 35,
-//    MVCrime = 80,
-//    MVDocumentary = 99,
-//    MVDrama = 18,
-//    MVFamily = 10751,
-//    MVFantasy = 14,
-//    MVHistory = 36,
-//    MVHorror = 27,
-//    MVMusic = 10402,
-//    MVMystery = 9648,
-//    MVRomance = 10749,
-//    MVScience_Fiction = 878,
-//    MVTV_Movie = 10770,
-//    MVThriller = 53,
-//    MVWar = 10752,
-//    MVWestern = 37
-//    
-//};
-
 #pragma mark -Initialisers
 - (instancetype) initWithMovie: (NSString *)movieName
                    andSummary:(NSString *)movieSummary
--- TVShowsOnTable/Models/TVSeries.h
+++ TVShowsOnTable/Models/TVSeries.h
@@ -14,28 +14,6 @@
 @property (strong, nonatomic) NSString *episode;
 @property (strong, nonatomic) NSString *summary;
 
-//#pragma mark -TV Genre enum
-//enum TvGenre {
-//
-//    TVActionAndAdventure = 10759,
-//    TVAnimation = 16,
-//    TVComedy = 35,
-//    TVCrime = 80,
-//    TVDocumentary = 99,
-//    TVDrama = 18,
-//    TVFamily = 10751,
-//    TVKids = 10762,
-//    TVMystery = 9648,
-//    TVNews = 10763,
-//    TVReality = 10764,
-//    TVSciFiAndFantasy = 10765,
-//    TVSoap = 10766,
-//    TVTalk = 10767,
-//    TVWarAndPolitics = 10768,
-//    TVWestern = 37
-//
-//};
-
 #pragma mark -initialisers
 - (instancetype)initWithEpisode: (NSString *)seriesEpisode
                      andSummary: (NSString *)summary
