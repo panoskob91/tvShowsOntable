c014a46b61a0fad8d8138f9663727d3c982757c1
panoskob91 <panoskob91@gmail.com>
2018-05-03 13:17:32 +0300

    youtube_trailer WIP

---
+++ TVShowsOnTable/ViewControllers/AFSEWebContentHandlerVC.h
@@ -0,0 +1,13 @@
+//
+//  AFSEWebContentHandlerVC.h
+//  TVShowsOnTable
+//
+//  Created by Panagiotis Kompotis on 03/05/2018.
+//  Copyright © 2018 AFSE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFSEWebContentHandlerVC : UIViewController
+
+@end
---
+++ TVShowsOnTable/ViewControllers/AFSEWebContentHandlerVC.m
@@ -0,0 +1,37 @@
+//
+//  AFSEWebContentHandlerVC.m
+//  TVShowsOnTable
+//
+//  Created by Panagiotis Kompotis on 03/05/2018.
+//  Copyright © 2018 AFSE. All rights reserved.
+//
+
+#import "AFSEWebContentHandlerVC.h"
+
+@interface AFSEWebContentHandlerVC ()
+
+@end
+
+@implementation AFSEWebContentHandlerVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
