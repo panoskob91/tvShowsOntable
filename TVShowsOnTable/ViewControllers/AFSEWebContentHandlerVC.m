38630b9ca69058cb0e386581d50dafe8a903f33a
panoskob91 <panoskob91@gmail.com>
2018-05-08 11:30:57 +0300

    web handler VC local fetching function removed

--- TVShowsOnTable/ViewControllers/AFSEWebContentHandlerVC.m
+++ TVShowsOnTable/ViewControllers/AFSEWebContentHandlerVC.m
@@ -54,42 +54,6 @@
     });
 }
 
-#pragma mark -Fetching
--(void)getYoutubeVideoKeyWithShowID:(NSNumber *)showId andMediaType:(NSString *)mediaType
-{
-    NSString *query = [NSString stringWithFormat:@"http://api.themoviedb.org/3/%@/%@/videos?api_key=6b2e856adafcc7be98bdf0d8b076851c",mediaType, showId];
-    NSURL *searchURL = [NSURL URLWithString:query];
-    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:searchURL];
-    NSURLSession *session = [NSURLSession sharedSession];
-    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData * data, NSURLResponse * response, NSError * error) {
-        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-        if (httpResponse.statusCode == 200)
-        {
-            NSError *parseError = nil;
-            NSMutableDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&parseError];
-            
-            if (![responseDictionary[@"results"] isEqual:[NSNull null]])
-            {
-                NSDictionary *resultsDictionary = responseDictionary[@"results"];
-                for (NSDictionary* result in resultsDictionary)
-                {
-                    if ([result[@"type"] isEqualToString:@"Trailer"])
-                    {
-                        self.youtubeVideoKey = result[@"key"];
-                    }
-                }
-            }
-            
-        }
-        else{
-            NSLog(@"ERROR %@", error);
-        }
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [self playVideoOnYoutubeWithKey:self.youtubeVideoKey];
-        });
-    }];
-    [dataTask resume];
-}
 
 - (void)playVideoOnYoutubeWithKey:(NSString *)youtubeVideoKey
 {
